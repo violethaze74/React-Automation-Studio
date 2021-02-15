4cc371bff9a086e8bfa7dbc5196f34c2e123c8dd
justinabraham <jusabra@gmail.com>
2021-02-15 09:52:19 +0200

    History collection created as empty collection

--- alarmHandlerDbInit/generateJSON.py
+++ alarmHandlerDbInit/generateJSON.py
@@ -6,11 +6,6 @@ with open('./initDBData/pvList.json') as f:
 pvs = {}
 pvsFile = []
 
-history = {}
-historyFile = []
-historyFile.append({"id": "_GLOBAL", "history": []})
-
-
 areaName = ''
 
 for area in data:
@@ -22,10 +17,6 @@ for area in data:
                 "area": areaName,
                 "enable": True,
                 "pvs": {}
-            }
-            history[areaName] = {
-                "id": areaName,
-                "history": []
             }
         elif(areaKey == "pvs"):
             # area pvs
@@ -39,27 +30,12 @@ for area in data:
                     "lastAlarmTime": "",
                     "lastAlarmAckTime": ""
                 }
-                if pvName not in history:
-                    history[pvName] = {
-                        "id": pvName,
-                        "areas": [areaName],
-                        "history": []
-                    }
-                else:
-                    areas=history[pvName]["areas"]
-                    newArea = areaName
-                    areas.append(newArea)
-                    history[pvName]["areas"] = list(set(areas))
         elif("subArea" in areaKey):
             # new subArea template
             pvs[areaName][areaKey] = {
                 "name": areaValue["name"],
                 "enable": True,
                 "pvs": {}
-            }
-            history[areaName+"="+areaValue["name"]] = {
-                "id": areaName+"="+areaValue["name"],
-                "history": []
             }
             # subArea pvs
             for pvKey, pvName in area[areaKey]["pvs"].items():
@@ -72,28 +48,14 @@ for area in data:
                     "lastAlarmTime": "",
                     "lastAlarmAckTime": ""
                 }
-                if pvName not in history:
-                    history[pvName] = {
-                        "id": pvName,
-                        "areas": [areaName+"="+areaValue["name"]],
-                        "history": []
-                    }
-                else:
-                    areas = history[pvName]["areas"]
-                    newArea = areaName+"="+areaValue["name"]
-                    areas.append(newArea)
-                    history[pvName]["areas"] = list(set(areas))
 
 for value in pvs.values():
     pvsFile.append(value)
-
-for value in history.values():
-    historyFile.append(value)
 
 with open('./initDBData/pvs.json', 'w') as json_file:
     json.dump(pvsFile, json_file)
 json_file.close()
 
-with open('./initDBData/history.json', 'w') as json_file:
-    json.dump(historyFile, json_file)
-json_file.close()
+# with open('./initDBData/history.json', 'w') as json_file:
+#     json.dump(historyFile, json_file)
+# json_file.close()
